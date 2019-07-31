0646858c3500c45b14bf5285a805bf235ff58c0a
Titus Moore <titusdmoore@gmail.com>
2019-07-31 16:21:17 -0400

    Added examples of other collections

---
+++ genericCollectionsProject/Customer.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace genericCollectionsProject {
+    public class Customers {
+        List<Customer> customerList = new List<Customer>();
+
+        public void LoadCustomers () {
+
+            // Example of alternate class definition
+            var cust1 = new Customer { Name = "P&G", State = "OH" };
+            var cust2 = new Customer { Name = "Microsoft", State = "WA" };
+            var cust3 = new Customer { Name = "Target", State = "MN" };
+
+            customerList.Add(cust1);
+            customerList.Add(cust2);
+            customerList.Add(cust3);
+        }
+
+        public void PrintCustomers() {
+            foreach (var cust in customerList) {
+                Console.WriteLine($"The Company {cust.Name} is based out of {cust.State}");
+            }
+        }
+
+    }
+    public class Customer {
+        public string Name { get; set; }
+        public string State { get; set; }
+
+
+    }
+}
--- genericCollectionsProject/Program.cs
+++ genericCollectionsProject/Program.cs
@@ -1,9 +1,63 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace genericCollectionsProject {
     class Program {
         static void Main(string[] args) {
-            Console.WriteLine("Hello World!");
+
+            var customers = new Customers();
+            customers.LoadCustomers();
+
+            customers.PrintCustomers();
+        }
+        static void Run() { 
+
+            List<int> scores = new List<int>( );
+
+            Random rnd = new Random();
+            int total = 0;
+            int highScore = 0;
+
+            do {
+                int listIdx = 0;
+                for (int i = 0; i < 10; i++) {
+                    scores.Add(rnd.Next(31));
+                }
+                foreach (var score in scores) {
+                    total += score;
+                }
+                if (highScore < total) {
+                    highScore = total;
+                } else {
+                    highScore = 0;        
+                }
+                total = 0;
+
+            } while (highScore < 294);
+
+            
+            Console.WriteLine($"Total score is {highScore}");
+            
+            
+            
+            //List<string> names = new List<string>();
+
+
+            //var integers = new List<int>();
+
+            //integers.Add(13);
+            //integers.Add(7);
+            //integers.Add(4);
+
+            //integers.Remove(4);
+
+            //integers.AddRange(new int[] { 4, 44, 444, 4444 });
+
+            //integers[3] = 99;
+
+            //foreach (var i in integers) {
+            //    Console.WriteLine($"i = {i}");
+            //}
         }
     }
 }
